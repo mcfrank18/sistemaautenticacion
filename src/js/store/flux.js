6dba7a6a00caddfa37b7590bfb0077b76de26353
mojonapower <maca95@gmail.com>
2021-12-16 22:52:25 +0000

    fixed

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,6 +1,7 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
+			logged: false,
 			demo: [
 				{
 					title: "FIRST",
@@ -15,6 +16,35 @@ const getState = ({ getStore, getActions, setStore }) => {
 			]
 		},
 		actions: {
+			enviarDatos: (e, mail, pass) => {
+				e.preventDefault();
+				console.log("mail", mail);
+				console.log("pass", pass);
+
+				var myHeaders = new Headers();
+				myHeaders.append("Content-Type", "application/json");
+
+				var raw = JSON.stringify({
+					email: mail,
+					password: pass
+				});
+
+				var requestOptions = {
+					method: "POST",
+					headers: myHeaders,
+					body: raw,
+					redirect: "follow"
+				};
+
+				fetch("https://3000-emerald-bee-81b0un6g.ws-us23.gitpod.io/user", requestOptions)
+					.then(response => response.json())
+					.then(result => {
+						console.log(result)
+						sessionStorage.setItem("token", result.token);
+						setStore({ logged: true });
+					})
+					.catch(error => console.log("error", error));
+			},
 			// Use getActions to call a function within a fuction
 			exampleFunction: () => {
 				getActions().changeColor(0, "green");
